7bbb05e6c1fc11f972fbb0ce7ec846ad7278f611
lc <1520788472@qq.com>
2019-03-24 23:13:56 +0800

    提交

---
+++ src/cn/tju/demo01/Student.java
@@ -0,0 +1,22 @@
+package cn.tju.demo01;
+
+public class Student {
+	private String name;
+	private int age;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setAge(int age){
+		this.age = age;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getAge(){
+		return age;
+	}
+}
